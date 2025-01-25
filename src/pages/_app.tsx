60e0a678732d3d9dba414ae4a7e3c366b7fb16db
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2025-01-25 16:34:31 -0500

    add oswald typeface and update sizing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider, DefaultTheme } from "styled-components";
 import GlobalStyle from "../components/globalstyles";
 import { Noto_Sans } from 'next/font/google';
+import { Oswald } from 'next/font/google';
 
 const noto = Noto_Sans({
 	weight: '400',
@@ -10,26 +11,42 @@ const noto = Noto_Sans({
 	subsets: ['latin'] // Adding the subset here
 });
 
+const oswald = Oswald({
+	weight: ['400', '700'],
+	style: 'normal',
+	subsets: ['latin'],
+});
+
 const theme: DefaultTheme = {
 	colors: {
-		background: "#161414",
-		text: "#ffffff",
-		accent: "#ba382c",
+		background: "#08080a",
+		text: "#dcdcdc",
+		accent: "#a32d15",
 	},
 	typography: {
 		fontFamily: noto.style.fontFamily,
+		headingFont: oswald.style.fontFamily,
 		fontWeight: {
 			thin: 200,
 			normal: 400,
 			bold: 700,
 		},
 		fontSize: {
-			heading: "2rem",
-			headingMobile: "1.2rem",
-			paragraph: "1.2rem",
+			heading: "2.5rem",
+			headingMobile: "1.6rem",
+			paragraph: "1.3rem",
 			paragraphMobile: "1rem",
-			uiCopy: "1.2rem",
+			uiCopy: "1.5rem",
 			uiCopyMobile: "1rem",
+		},
+		lineHeight: {
+			heading: "1.3",
+			paragraph: "1.6",
+		},
+		letterSpacing: {
+			tight: "-0.03em",
+			normal: "0",
+			wide: "0.1em",
 		},
 	},
 };
