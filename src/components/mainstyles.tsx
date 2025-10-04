96579a7c1047413dd223a6a777c6a68a47eb9a92
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2025-10-04 12:06:35 -0400

    refactor: improve styling and add breakpoints

--- src/components/mainstyles.tsx
+++ src/components/mainstyles.tsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+// Centralized breakpoints for consistency
+const breakpoints = {
+    phone: "512px",
+    tablet: "768px",
+    smallLaptop: "820px",
+} as const;
+
 const Container = styled.div`
     display: flex;
     flex-flow: column nowrap;
@@ -10,7 +17,7 @@ const Container = styled.div`
     margin: 0 auto;
     padding: 2rem 4%;
 
-    @media (max-width: 768px) {
+    @media (max-width: ${breakpoints.tablet}) {
         padding: 1.5rem 3%;
     }
 `;
@@ -22,184 +29,200 @@ const Main = styled.main`
     align-self: center;
     justify-content: flex-start;
     flex: 1;
-    width: 98%;
+    width: 100%;
     max-width: 1600px;
     height: auto;
 
-    @media only screen and (max-width: 820px) {
+    /** Small laptops and equivalent devices */
+    @media only screen and (max-width: ${breakpoints.smallLaptop}) {
         width: 100%;
     }
+
     /** Generic tablet and equivalent devices */
-    @media only screen and (max-width: 768px) {
+    @media only screen and (max-width: ${breakpoints.tablet}) {
         width: 98%;
         margin: 0 auto;
     }
+
     /** iPhone portrait mode and equivalent devices */
-    @media only screen and (max-width: 512px) {
+    @media only screen and (max-width: ${breakpoints.phone}) {
         width: 96%;
     }
 `;
 
 const Nav = styled.nav`
-	position: relative;
-	display: flex;
-	flex-flow: row nowrap;
-	justify-content: space-between;
-	align-self: center;
-	align-items: baseline;
-	width: 100%;
-	height: auto;
+    position: relative;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-self: center;
+    align-items: baseline;
+    width: 100%;
+    height: auto;
 
-	.controls {
-		width: 100%;
-		display: flex;
-		flex-flow: row nowrap;
-		justify-content: space-between;
-		margin: 0.5rem 0;
-		font-size: ${({ theme }) => theme.typography.fontSize.uiCopy};
-		
+    .controls {
+        width: 100%;
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+        margin: 0.5rem 0;
+        font-size: ${({ theme }) => theme.typography.fontSize.uiCopy};
 
-		/** iPhone portrait mode and equivalent devices */
-    @media only screen and (max-width: 512px) {
-		align-items: center;
-        font-size: ${({ theme }) => theme.typography.fontSize.uiCopyMobile};
+        /** iPhone portrait mode and equivalent devices */
+        @media only screen and (max-width: ${breakpoints.phone}) {
+            align-items: center;
+            font-size: ${({ theme }) => theme.typography.fontSize.uiCopyMobile};
+        }
     }
 
-	}
+    a {
+        color: ${({ theme }) => theme.colors.text};
+        text-decoration: none;
+        transition:
+            color 120ms ease-in-out,
+            text-underline-offset 120ms ease-in-out;
 
-	a {
-		color: ${({ theme }) => theme.colors.text};
-		&:hover,
-        :focus,
-        :active {
-			color: ${({ theme }) => theme.colors.accent};
-            text-decoration: underline solid 0.8rem;
+        &:hover,
+        &:focus-visible,
+        &:active {
+            color: ${({ theme }) => theme.colors.accent};
+            text-decoration: underline;
+            text-decoration-thickness: 0.08rem;
+            text-underline-offset: 0.2rem;
         }
-		padding: 0 1rem 0 0;
-		
-		&:last-child {
-			padding: 0;
-		}
-	}
+        padding: 0 1rem 0 0;
 
-	@media screen and (max-width: 512px) {
-		width: 98%;
-	}
+        &:last-child {
+            padding: 0;
+        }
+    }
+
+    @media screen and (max-width: ${breakpoints.phone}) {
+        width: 98%;
+    }
 `;
 
 const H1 = styled.h1`
-	position: relative;
-	display: inline-flex;
+    position: relative;
+    display: inline-flex;
     line-height: 1.25;
     font-size: ${({ theme }) => theme.typography.fontSize.heading};
     text-align: center;
     text-decoration: none;
-	margin: 0 auto;
-	max-width: 24rem;
+    margin: 0 auto;
+    max-width: 24rem;
 
-	/** iPad portrait mode and equivalent devices */
-    @media only screen and (max-width: 768px) {
-		margin: 2rem auto;
+    /** iPad portrait mode and equivalent devices */
+    @media only screen and (max-width: ${breakpoints.tablet}) {
+        margin: 2rem auto;
     }
 
     /** iPhone portrait mode and equivalent devices */
-    @media only screen and (max-width: 512px) {
+    @media only screen and (max-width: ${breakpoints.phone}) {
         font-size: ${({ theme }) => theme.typography.fontSize.headingMobile};
     }
 `;
 
 const H2 = styled.h2`
-	z-index: 2;
-  	position: relative;
-  	display: flex;
-	flex-flow: column nowrap;
-	align-items: center;
-	justify-content: center;
-	align-self: center;
-  	width: 24rem;
-  	font-size: ${({ theme }) => theme.typography.fontSize.heading};
-  	text-align: justify;
-  	margin: 200px auto 0px;
-  	text-transform: uppercase;
+    z-index: 2;
+    position: relative;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+    align-self: center;
+    width: 24rem;
+    font-size: ${({ theme }) => theme.typography.fontSize.heading};
+    text-align: justify;
+    margin: 12.5rem auto 0; /* 200px -> 12.5rem for scalability */
+    text-transform: uppercase;
 
-	/** iPad portrait mode and equivalent devices */
-    @media only screen and (max-width: 768px) {
+    /** iPad portrait mode and equivalent devices */
+    @media only screen and (max-width: ${breakpoints.tablet}) {
         font-size: ${({ theme }) => theme.typography.fontSize.headingMobile};
     }
 
-  	@media screen and (max-width: 512px) {
-    	width: 16rem;
-	}
+    @media screen and (max-width: ${breakpoints.phone}) {
+        width: 16rem;
+    }
 `;
 
 const RedBlock = styled.span`
-  position: relative;
-  display: flex;
-  flex-flow: column nowrap;
-  align-self: center;
-  width: 100%;
-  height: auto;
-
-  &::before {
-    content: '';
-    z-index: 1;
-    display: block;
-    position: absolute;
+    position: relative;
+    display: flex;
+    flex-flow: column nowrap;
+    align-self: center;
     width: 100%;
-    height: 83%;
-    background-color: ${({ theme }) => theme.colors.accent};
-  }
+    height: auto;
 
-  @media screen and (max-width: 512px) {
-    width: 98%;
-  }
+    &::before {
+        content: "";
+        z-index: 1;
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 83%;
+        background-color: ${({ theme }) => theme.colors.accent};
+    }
+
+    @media screen and (max-width: ${breakpoints.phone}) {
+        width: 98%;
+    }
 `;
 
 const P = styled.p`
-	max-width: 696px;
+    max-width: 696px;
     text-align: left;
-	margin: 1.618rem auto;
+    margin: 1.618rem auto;
     line-height: 1.618;
     font-size: ${({ theme }) => theme.typography.fontSize.paragraph};
-	letter-spacing: 0.01618rem;
+    letter-spacing: 0.01618rem;
 
-	a {
+    a {
         color: ${({ theme }) => theme.colors.accent};
+        text-decoration: underline;
+        text-underline-offset: 0.12rem;
         &:hover,
-        :focus,
-        :active {
-            text-decoration: underline solid 0.3rem;
+        &:focus-visible,
+        &:active {
+            text-decoration-thickness: 0.3rem;
         }
     }
 
-	@media screen and (max-width: 512px) {
-    	font-size: ${({ theme }) => theme.typography.fontSize.paragraphMobile};
-		margin: 1rem auto;
-		text-align: justify;
-		line-height: 1.318;
-	}
+    @media screen and (max-width: ${breakpoints.phone}) {
+        font-size: ${({ theme }) => theme.typography.fontSize.paragraphMobile};
+        margin: 1rem auto;
+        text-align: justify;
+        line-height: 1.318;
+    }
 `;
 
 const Footer = styled.footer`
-	display: flex;
-	flex-flow: row nowrap;
-	align-items: center;
-	justify-content: space-between;
-	align-self: center;
-	width: 100%;
-	height: 8rem;
-	margin: 0 auto;
-	padding: 0;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: space-between;
+    align-self: center;
+    width: 100%;
+    height: 8rem;
+    margin: 0 auto;
+    padding: 0;
 
-	a {
-		color: ${({ theme }) => theme.colors.text};
-		&:hover,
-        :focus,
-        :active {
-			color: ${({ theme }) => theme.colors.accent};
-            text-decoration: underline solid 1rem;
+    a {
+        color: ${({ theme }) => theme.colors.text};
+        text-decoration: none;
+        transition:
+            color 120ms ease-in-out,
+            text-underline-offset 120ms ease-in-out;
+        &:hover,
+        &:focus-visible,
+        &:active {
+            color: ${({ theme }) => theme.colors.accent};
+            text-decoration: underline;
+            text-decoration-thickness: 0.1rem;
+            text-underline-offset: 0.2rem;
         }
-	}
+    }
 `;
 
 export { Container, Main, H1, Nav, H2, RedBlock, P, Footer };
