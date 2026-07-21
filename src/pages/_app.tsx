c08004d91652f3a2f2a015383915dfa5a927dc15
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2026-07-20 20:02:00 -0400

    chore: fix CI typecheck and slim GitHub Actions
    Remove styled-jsx from _app (types broke under Next 16) and apply
    next/font on body instead. Bump CI actions to latest majors, use Node
    22 with built-in pnpm cache, retarget Dependabot to main, and close
    stale action/next bumps. CodeQL default setup disabled for this site.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,13 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "@/components/globalstyles";
-import { theme, noto } from "@/config/theme";
+import { theme } from "@/config/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <>
-            {/* Early font-family to reduce FOUC; GlobalStyle (below) also sets it from theme.typography.fontFamily */}
-            <style jsx global>{`
-                html {
-                    font-family: ${noto.style.fontFamily};
-                }
-            `}</style>
-            <ThemeProvider theme={theme}>
-                <GlobalStyle />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </ThemeProvider>
     );
 }
